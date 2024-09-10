8d491998669567dd418c477dab9b78a6f5386112
ZwakeleMahlalela <157624892+ZwakeleMahlalela@users.noreply.github.com>
2024-09-10 12:05:08 +0200

    changed the input "text" to input "search"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function handleSearchSubmit(event) {
-    event.preventDefault
-let searchInput = document.querySelector("#search-form-input"); 
+    event.preventDefault();
+let searchInput = document.querySelector("#search-input"); 
 let cityElement = document.querySelector("#current-city");
 cityElement.innerHTML = searchInput.value;
 }

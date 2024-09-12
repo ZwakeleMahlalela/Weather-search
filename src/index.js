e5fd5309cbe3a24d87608ebed22661f50fd5e610
ZwakeleMahlalela <157624892+ZwakeleMahlalela@users.noreply.github.com>
2024-09-12 11:13:55 +0200

    Injected weather data

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ function refreshWeather(response){
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
 console.log(response.data);
 
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji"/>`;
     cityElement.innerHTML = response.data.city;
-
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
@@ -27,7 +28,11 @@ function formatDate(date) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
 
-  return `${day}  ${hours} ${minutes}`
+    if (minutes < 10){
+        minutes =`0${minutes}`
+    }
+
+  return `${day}  ${hours}:${minutes}`
 }
 
 function searchCity(city) {
